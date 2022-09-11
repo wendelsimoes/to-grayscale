1b1fb6b0fea64ddbd2b73ea2ddfcd48cf5117ed2
wendelsimoes <wendel.simoes.br@gmail.com>
2022-09-10 23:03:18 -0300

    Filtering working successfully

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+string currentDirectory = Directory.GetCurrentDirectory();
+Bitmap bt = new Bitmap(Path.Combine(currentDirectory, "example-in.jpg"));
+
+for (int y = 0; y < bt.Height; y++)
+{
+    for (int x = 0; x < bt.Width; x++)
+    {
+        Color c = bt.GetPixel(x, y);
+
+        int r = c.R;
+        int g = c.G;
+        int b = c.B;
+        int avg = (r + g + b) / 3;
+        bt.SetPixel(x, y, Color.FromArgb(avg,avg,avg));
+    }   
+}
+
+bt.Save(Path.Combine(currentDirectory, "example-out.jpg"));
